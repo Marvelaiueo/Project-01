d6d27b5ceb546dc92c39c7e4c80e727665f85b8b
Marvelaiueo <yasaumbara27@gmail.com>
2024-09-29 12:58:08 +0700

    First commit

--- app.py
+++ app.py
@@ -22,36 +22,40 @@ def home():
 
 @app.route('/diary', methods=['GET'])
 def show_diary():
-    articles = list(db.diary.find({},{'_id':False}))
+    articles = list(db.diary.find({}, {'_id': False}))
     return jsonify({'articles': articles})
 
 @app.route('/diary', methods=['POST'])
 def save_diary():
     title_receive = request.form.get('title_give')
     content_receive = request.form.get('content_give')
-    
+
     today = datetime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
 
+    static_folder = 'static'
+    if not os.path.exists(static_folder):
+        os.makedirs(static_folder)
+
     file = request.files["file_give"]
     extension = file.filename.split('.')[-1]
-    filename = f'static/post-{mytime}.{extension}'
+    filename = f'{static_folder}/post-{mytime}.{extension}'
     file.save(filename)
-    
+
     profile = request.files['profile_give']
     extension = profile.filename.split('.')[-1]
-    profilename = f'static/profile-{mytime}.{extension}'
+    profilename = f'{static_folder}/profile-{mytime}.{extension}'
     profile.save(profilename)
-   
+
     doc = {
         'file': filename,
         'profile': profilename,
         'title': title_receive,
-        'content':content_receive
+        'content': content_receive
     }
     db.diary.insert_one(doc)
 
-    return jsonify({'msg':'Upload complete!'})
+    return jsonify({'msg': 'Upload complete!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
